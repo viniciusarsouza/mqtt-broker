72af4384fcce14c8a9df8247a69646e1ddf19c3e
viniciusarsouza <viniciusarsouza@gmail.com>
2022-03-23 11:37:30 -0300

    feat: create publish/subscribe with iot core

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ const device = awsIot.device({
   caPath: 'root-CA.crt',
   clientId: 'temp-sensor',
   region: 'us-west-2',
-  host: 'a27sf0y6gli8so.iot.us-west-2.amazonaws.com'
+  host: 'a27sf0y6gli8so-ats.iot.us-west-2.amazonaws.com'
 })
 
-const contents = "Started!!!"
 
-device.on('connect', function() {
-  console.log('connect');
+device
+  .on('connect', function() {
+    console.log('connect');
 
-  device.publish('temp-sensor-Policy', JSON.stringify({test_data: 'NodeJS'}))
+    
 
-  console.log('Message Sent')
-})
+    const payload = {
+        temperature: 21 
+    }
 
-device.on('message', function(topic, payload) {
-  console.log('message', topic, payload.toString())
-})
+    device.subscribe('sensor/data/temperature');
+    device.subscribe('sensor/data/calibration');
+    device.publish('sensor/data', JSON.stringify(payload))
+    device.publish('sensor/calibrate', JSON.stringify({}))
+  });
+
+device
+  .on('message', function(topic, payload) {
+    console.log('payload: ', payload.toString() );
+  });
