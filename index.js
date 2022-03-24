5f47d66d69289fab1607c2ae4e0302f332363339
viniciusarsouza <viniciusarsouza@gmail.com>
2022-03-23 22:27:36 -0300

    feat: receiving message from iot core

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 const awsIot = require('aws-iot-device-sdk')
+var Chance = require('chance');
 
 const device = awsIot.device({
-  keyPath: 'temp-sensor.private.key',
-  certPath: 'temp-sensor.cert.pem',
-  caPath: 'root-CA.crt',
+  keyPath: './util/temp-sensor.private.key',
+  certPath: './util/temp-sensor.cert.pem',
+  caPath: './util/root-CA.crt',
   clientId: 'temp-sensor',
   region: 'us-west-2',
   host: 'a27sf0y6gli8so-ats.iot.us-west-2.amazonaws.com'
 })
 
+const chance = new Chance()
 
 device
-  .on('connect', function() {
+  .on('connect', async function() {
     console.log('connect');
 
-    
+    device.subscribe('sensor/data/calibration');
 
-    const payload = {
-        temperature: 21 
+    for(let i = 0; i < 60; i++){
+      const payload = {
+        temperature: chance.floating({ min: 20, max: 22 }),
+        call_calibration: false
     }
 
-    device.subscribe('sensor/data/temperature');
-    device.subscribe('sensor/data/calibration');
-    device.publish('sensor/data', JSON.stringify(payload))
-    device.publish('sensor/calibrate', JSON.stringify({}))
+    if(i == 58) {
+      payload.call_calibration = true
+    }
+
+    console.log(`Seconds: ${i +1 } -> Temperature: ${payload.temperature}`)
+
+    await device.publish('sensor/data', JSON.stringify(payload))
+    }
+
   });
+
+
 
 device
   .on('message', function(topic, payload) {
-    console.log('payload: ', payload.toString() );
+    console.log('y = m*x + b -> ', payload.toString())
   });
+
+
+  
